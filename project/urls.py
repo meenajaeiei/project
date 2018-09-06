798bd3a2b97018e2371535699a0fc85e564979b0
mheelnwza007 <59070118@it.kmitl.ac.th>
2018-09-06 23:05:57 +0700

    building env

--- project/urls.py
+++ project/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from .views import home_page
+from .views import home_page,user_login
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('adminx/', admin.site.urls),
+    path('admin/', admin.site.urls),
     #path('bookmarks/', include('bookmarks.urls')),
     path(r'', home_page),
+    path(r'login/', user_login, name='login'),
 ]
+
+if settings.DEBUG:
+    urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
