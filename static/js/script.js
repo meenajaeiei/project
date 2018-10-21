af4fee205c04855e703d6ea94ed0daf3ce656fd6
pp <59070093@it.kmitl.ac.th>
2018-10-21 11:26:39 +0700

    edit detail box

--- static/js/script.js
+++ static/js/script.js
@@ -1,64 +1,24 @@
-//set global variable
-
-
-// var header = document.getElementById("floor-side");
-// var allRooms = document.querySelectorAll("g")[0];
-// for (var i = 0; i < floorSelected.length; i++) {
-// 	floorSelected[i].addEventListener("click", function() {
-// 		var current = document.getElementsByClassName("active");
-// 		if (current.length > 0) { 
-// 			current[0].className = current[0].className.replace(" active", "");
-// 		}
-// 		this.className += " active";
-
-// 	});
-// }
-
-// function showFloor(floorNumber) {
-
-// 	if(floorNumber == "floor-1" || floorNumber == "floor-m") {
-// 		document.getElementById("floor-1-m-map").style.display = "block";
-// 		document.getElementById("floor-2-map").style.display = "none";
-// 		document.getElementById("floor-3-map").style.display = "none";
-// 	} else if (floorNumber == "floor-2") {
-// 		document.getElementById("floor-2-map").style.display = "block";
-// 		document.getElementById("floor-1-m-map").style.display = "none";
-// 		document.getElementById("floor-3-map").style.display = "none";
-// 	} else {
-// 		document.getElementById("floor-3-map").style.display = "block";
-// 		document.getElementById("floor-1-m-map").style.display = "none";
-// 		document.getElementById("floor-2-map").style.display = "none";
-// 	}
-
-// }
-
 function showDetail(roomNumber , roomstatus) {
 
 	document.getElementById("close-detail").style.display = "block";
 	var roomNumberID = document.getElementById("room-number");
 	var statusID = document.getElementById("status");
 	var datetime = document.getElementsByClassName("datetime");
-	var buttonID = document.getElementById("eiei");
+	var buttonID = document.getElementById("reserve");
 	document.getElementById("demo").value =  roomNumber;
 	roomNumberID.innerHTML = roomNumber;
 	statusID.innerHTML = roomstatus;
 	
 	for(i=0;i<datetime.length; i++) {
-		datetime[i].style.display = "block";
+		datetime[i].style.display = "inline-block";
 	}
-	roomNumberID.style.display = "block";
-	statusID.style.display = "block";
+	roomNumberID.style.display = "inline-block";
+	statusID.style.display = "inline-block";
 	buttonID.style.display = "block";
 	var status = statusID.innerText;
-	
 
-
-
-	document.getElementById("detail-box").style.width = "45%";
-	document.getElementById("detail-box").style.height = "65vh";
-	
-
-
+	document.getElementById("detail-box").style.width = "40%";
+	document.getElementById("detail-box").style.height = "55vh";
 
 }
 
@@ -70,7 +30,7 @@ function exit() {
 		datetime[i].style.display = "none";
 	}
 	document.getElementById("status").style.display = "none";
-	document.getElementById("eiei").style.display = "none";
+	document.getElementById("reserve").style.display = "none";
 	document.getElementById("detail-box").style.width = "0%";
 	document.getElementById("detail-box").style.height = "0%";
 }
