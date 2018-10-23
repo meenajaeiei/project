e21251b437d32ed72ecd2f3a43508540d69a6b9d
pp <59070093@it.kmitl.ac.th>
2018-10-23 16:37:17 +0700

    select floor

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,30 @@
+// var slideIndex = 1;
+// showDivs(slideIndex);
+
+// function plusDivs(n) {
+//   showDivs(slideIndex += n);
+// }
+
+// function currentDiv(n) {
+//   showDivs(slideIndex = n);
+// }
+
+// function showDivs(n) {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("demo");
+//   if (n > x.length) {slideIndex = 1}    
+//   if (n < 1) {slideIndex = x.length}
+//   for (i = 0; i < x.length; i++) {
+//      x[i].style.display = "none";  
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//      dots[i].className = dots[i].className.replace(" w3-white", "");
+//   }
+//   x[slideIndex-1].style.display = "block";  
+//   dots[slideIndex-1].className += " w3-white";
+// }
+
 function showDetail(roomNumber , roomstatus) {
 
 	document.getElementById("close-detail").style.display = "block";
@@ -34,3 +61,4 @@ function exit() {
 	document.getElementById("detail-box").style.width = "0%";
 	document.getElementById("detail-box").style.height = "0%";
 }
+
