af888e75e5c4aff786148e6117169c032ab0e5c5
pp <59070093@it.kmitl.ac.th>
2018-10-28 01:44:33 +0700

    add pop-up detail box at status.html

--- static/js/script.js
+++ static/js/script.js
@@ -1,30 +1,3 @@
-// var slideIndex = 1;
-// showDivs(slideIndex);
-
-// function plusDivs(n) {
-//   showDivs(slideIndex += n);
-// }
-
-// function currentDiv(n) {
-//   showDivs(slideIndex = n);
-// }
-
-// function showDivs(n) {
-//   var i;
-//   var x = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("demo");
-//   if (n > x.length) {slideIndex = 1}    
-//   if (n < 1) {slideIndex = x.length}
-//   for (i = 0; i < x.length; i++) {
-//      x[i].style.display = "none";  
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//      dots[i].className = dots[i].className.replace(" w3-white", "");
-//   }
-//   x[slideIndex-1].style.display = "block";  
-//   dots[slideIndex-1].className += " w3-white";
-// }
-
 function showDetail(roomNumber , roomstatus) {
 
 	document.getElementById("close-detail").style.display = "block";
@@ -32,27 +5,41 @@ function showDetail(roomNumber , roomstatus) {
 	var statusID = document.getElementById("status");
 	var datetime = document.getElementsByClassName("datetime");
 	var buttonID = document.getElementById("reserve");
+	var paragraph = document.getElementsByClassName("text-in-detail-box");
 	document.getElementById("demo").value =  roomNumber;
+
+	for(i=0;i<paragraph.length; i++) {
+		paragraph[i].style.display = "block";
+	}
+
 	roomNumberID.innerHTML = roomNumber;
 	statusID.innerHTML = roomstatus;
 	
 	for(i=0;i<datetime.length; i++) {
 		datetime[i].style.display = "inline-block";
 	}
-	roomNumberID.style.display = "inline-block";
+
+	
+	roomNumberID.style.display = "block";
 	statusID.style.display = "inline-block";
 	buttonID.style.display = "block";
 	var status = statusID.innerText;
 
 	document.getElementById("detail-box").style.width = "40%";
-	document.getElementById("detail-box").style.height = "55vh";
+	document.getElementById("detail-box").style.height = "auto";
 
 }
 
 function exit() {
 	document.getElementById("close-detail").style.display = "none";
 	document.getElementById("room-number").style.display = "none";
+	var paragraph = document.getElementsByClassName("text-in-detail-box");
 	var datetime = document.getElementsByClassName("datetime");
+
+	for(i=0;i<paragraph.length; i++) {
+		paragraph[i].style.display = "none";
+	}
+
 	for(i=0;i<datetime.length; i++) {
 		datetime[i].style.display = "none";
 	}
